ce87da88058e4ac222ea2f2cfcc5a05e88cc78ef
marconeri70 <marconeri70@gmail.com>
2025-09-22 12:57:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("v1k-cache").then(cache => cache.addAll([
-    "/", "/index.html", "/checklist.html", "/style.v1k.css", "/app.v1k.js"
-  ])));
+const CACHE = "skf5s-v1k";
+const ASSETS = ["/","/index.html","/checklist.html","/style.v1k.css","/app.v1k.js"];
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
 });
