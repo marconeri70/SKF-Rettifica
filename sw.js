acbfbd72e9ddde69a7ba912aac2dc08861661556
marconeri70 <marconeri70@gmail.com>
2025-09-22 15:45:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,29 @@
-const CACHE = "skf5s-v1k-ch24";
-const ASSETS = ["./","./index.html","./checklist.html","./style.v1k.css","./app.v1k.js"];
+const SW_VERSION = 'skf5s-sw-v7.17.10';
+const CORE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './assets/5s-hero.png',
+  './assets/skf-192.png',
+  './assets/skf-512.png'
+];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(SW_VERSION).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
 });
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==SW_VERSION).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+  );
 });
-self.addEventListener("fetch", e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+self.addEventListener('fetch', e=>{
+  const req=e.request;
+  e.respondWith(
+    caches.match(req).then(res=> res || fetch(req).then(r=>{
+      const copy=r.clone(); caches.open(SW_VERSION).then(c=>c.put(req,copy)); return r;
+    }).catch(()=>caches.match('./')) )
+  );
 });
