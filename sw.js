072f5a97880e82d31b493ac9c021a4659c7af7c5
marconeri70 <marconeri70@gmail.com>
2025-09-22 19:31:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,28 @@
-const CACHE = "skf5s-v1k";
+// SW minimale per cache statica
+const CACHE = 'skf5s-v1k';
 const ASSETS = [
-  "./",
-  "index.html",
-  "style.css",
-  "app.js",
-  "manifest.json",
-  "assets/skf-192.png",
-  "assets/skf-512.png",
-  "assets/5s-hero.png"
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  // le immagini restano nella tua cartella assets/
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener("activate", e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+  )));
+  self.clients.claim();
 });
-self.addEventListener("fetch", e=>{
-  const url = new URL(e.request.url);
-  if(url.origin === location.origin){
-    e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request)));
-  }
+
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=> r || fetch(e.request))
+  );
 });
