d38a167004b197ea5e31032cdd3c5bbe7a920d20
marconeri70 <marconeri70@gmail.com>
2025-09-22 17:04:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-
-const CACHE='skf5s-v1kch24';
-const ASSETS=[
-  './',
-  './index.html',
-  './checklist.html',
-  './style.v1k.css',
-  './app.v1k.js',
-  './manifest.json'
+const CACHE = "skf5s-v1k-ch24";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./checklist.html",
+  "./style.v1k.css",
+  "./app.v1k.js",
+  "./manifest.json"
 ];
-self.addEventListener('install', e=>{
+
+self.addEventListener("install", e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-self.addEventListener('activate', e=>{
+self.addEventListener("activate", e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
-self.addEventListener('fetch', e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
